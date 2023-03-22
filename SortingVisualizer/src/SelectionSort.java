520d5956e7f98c8130abdcb4dad85dd3ed6e45d0
Ahmad Masud <ahmadmasud25@hotmail.com>
2023-03-21 22:07:59 -0700

    Add files via upload

---
+++ SortingVisualizer/src/SelectionSort.java
@@ -0,0 +1,26 @@
+public class SelectionSort implements Panel.SortingAlgorithm {
+    
+    private Panel visualizer;
+
+    public SelectionSort(Panel visualizer) {
+        this.visualizer = visualizer;
+    }
+    
+    public void sort(int[] list){  
+        for (int i = 0; i < list.length - 1; i++)  {  
+            int index = i;  
+            for (int j = i + 1; j < list.length; j++){  
+                if (list[j] < list[index]){  
+                    index = j;
+                    if (visualizer.threadInterrupted()) return;
+                    visualizer.animate(i, 2500000);
+                }  
+            }  
+            int smallerNumber = list[index];   
+            list[index] = list[i];  
+            list[i] = smallerNumber;  
+        }
+        visualizer.checkSorted(list);   
+        visualizer.buttonsEnabled(true);
+    }  
+}
